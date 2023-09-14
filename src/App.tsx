aa432b39d44621b231e4e827587a0815f8d08b3e
Julia <veluat@gmail.com>
2023-09-14 12:43:16 +0300

    fix: App content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
+import { Box } from "@mui/material";
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
 import React from "react";
+import { TreeView } from "@mui/x-tree-view";
+import { TreeComponent } from "./TreeComponent";
+import { selectTreeData } from "./treeSlice";
+import { useSelector } from "react-redux";
+
+export type TreeNodeType = {
+    id: string;
+    name: string;
+    parentId?: string;
+    children?: TreeNodeType[];
+};
 
 export function App() {
+    const treeData = useSelector(selectTreeData);
 
     return (
-        <div>Tree</div>
+        <Box sx={{ height: '100vh', width: '100vw' }}>
+            <TreeView
+                aria-label="rich object"
+                defaultCollapseIcon={<ExpandMoreIcon />}
+                defaultExpanded={['root']}
+                defaultExpandIcon={<ChevronRightIcon />}
+            >
+                {treeData.map((el: TreeNodeType) => (
+                    <TreeComponent key={el.id.toString()} nodes={el} isRoot nodeId={el.id.toString()} />
+                ))}
+            </TreeView>
+        </Box>
     );
 }
