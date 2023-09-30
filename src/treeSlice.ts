3ff7f20a465f40ae04ce7d3caecdfa1d2d5984be
Julia <veluat@gmail.com>
2023-09-30 12:53:09 +0300

    feat: added helper functions to update the tree data

--- src/treeSlice.ts
+++ src/treeSlice.ts
@@ -54,3 +54,68 @@ const treeSlice = createSlice({
         },
     },
 });
+
+// Helper functions to update the tree data
+
+const deleteNodeFromTree = (treeData: Node[], nodeId: string): Node[] => {
+    return treeData.flatMap((node) => {
+        if (node.id === nodeId) {
+            return [];
+        } else if (node.children) {
+            return [
+                {
+                    ...node,
+                    children: deleteNodeFromTree(node.children, nodeId),
+                },
+            ];
+        }
+        return [node];
+    });
+};
+
+const addNodeToTree = (treeData: Node[], parentId: string, newNode: Node): Node[] => {
+    return treeData.map((node) => {
+        if (node.id === parentId) {
+            if (node.children) {
+                return {
+                    ...node,
+                    children: [...node.children, newNode],
+                };
+            } else {
+                return {
+                    ...node,
+                    children: [newNode],
+                };
+            }
+        } else if (node.children) {
+            return {
+                ...node,
+                children: addNodeToTree(node.children, parentId, newNode),
+            };
+        }
+        return node;
+    });
+};
+
+const renameNodeInTree = (treeData: Node[], nodeId: string, newName: string): Node[] => {
+    return treeData.map((node) => {
+        if (node.id === nodeId) {
+            return {
+                ...node,
+                name: newName,
+            };
+        } else if (node.children) {
+            return {
+                ...node,
+                children: renameNodeInTree(node.children, nodeId, newName),
+            };
+        }
+        return node;
+    });
+};
+
+export const { deleteNode, addNode, renameNode } = treeSlice.actions;
+
+export const selectTreeData = (state: RootState) => state.tree.treeData;
+
+export default treeSlice.reducer;
