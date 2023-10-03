54c6285633a801c05543e88c0da3a7a4c995bf61
Julia <veluat@gmail.com>
2023-10-03 13:10:21 +0300

    fix: Provider added

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,13 @@ import {StrictMode} from 'react'
 
 import {createRoot} from 'react-dom/client'
 import {App} from "./App";
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 createRoot(document.getElementById('root') as HTMLElement).render(
     <StrictMode>
-        <App/>
+        <Provider store={store}>
+            <App/>
+        </Provider>
     </StrictMode>
 )
